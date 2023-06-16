f6cc883fadd2eae376bc9ffcb8c188e86727c81d
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-16 11:16:05 -0400

    working on functions

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+/*----- constants -----*/
+
+
+  /*----- state variables -----*/
+let guess ;  // -- current guess
+let guesses = []; // -- how many guesses are left
+let lives ; // -- how many lives are left
+let blank ; // -- the blank space used for the correct letters
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+  /*----- cached elements  -----*/
+let livesLeftEl = document.getElementById("lives");
+let lettersEl = document.getElementById("letters");
+let getHintEl = document.getElementById("hints");
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+
+  let restart = function () {
+    buttonfunc = document.getElementById("playagain");
+
+
+
+  }
+
+
+  
