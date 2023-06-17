c6c3dd76183602d5e9cb66a0829f7a59fef83ed6
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-17 12:19:06 -0400

    working win-lose logic

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
  
 let guess ;  // -- current guess
 let guesses = []; // -- how many guesses are left
-let lives ; // -- how many lives are left
+let lives = 10 ; // -- how many lives are left
 let blank ; // -- the blank space used for the correct letters
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let dictionary = ["toyota", "honda", "subaru", "mazda"];
@@ -26,6 +26,30 @@ function refreshPage(){
     window.location.reload();
 } 
 
+// array.from
 
+let rndmWordArray = Array.from(word);
 
 console.log(word);
+console.log(rndmWordArray);
+
+
+function guessHandle() {
+if (rndmWordArray.contains(userInput)) {
+    guesses.push(userInput);
+
+} else {
+ guesses.push(userInput);
+ lives--;
+ console.log(lives)
+}
+
+
+}
+
+function loseCondition(lives) {
+if (lives === 0);
+return alert('YOU HAVE LOST!');
+
+
+}
