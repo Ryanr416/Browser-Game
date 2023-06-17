89b398cbdd4e4d1a83ec885bbd60d76a9646176f
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-17 13:49:02 -0400

    working on buttons

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+
+
+
 /*----- constants -----*/
 
 
@@ -12,14 +16,56 @@ let dictionary = ["toyota", "honda", "subaru", "mazda"];
 var selectedWord;
 var rndmNum;
 var word = dictionary[Math.floor(Math.random() * dictionary.length)];
+var winWordArray = [];
+var myButtons;
+var letters;
   /*----- cached elements  -----*/
 let livesLeftEl = document.getElementById("lives");
 let lettersEl = document.getElementById("letters");
 let getHintEl = document.getElementById("hints");
 
 
-  /*----- event listeners -----*/
+//switch pics each time you lose a life
+var hangmanPic = function() {
+if (lives === 5 ) {
+<img src="https://hangman.doncolton.com/play/hang0.png"></img>
+}
+}
 
+var buttons = function () {
+  myButtons = document.getElementById('buttons');
+  letters = document.createElement('ul');
+
+  for (var i = 0; i < alphabet.length; i++) {
+    letters.id = 'alphabet';
+    list = document.createElement('li');
+    list.id = 'letter';
+    list.innerHTML = alphabet[i];
+    myButtons.appendChild(letters);
+    letters.appendChild(list);
+  }
+}
+// dynamically rendering elements
+
+
+// why doesnt my buttons element render in the browser
+var dashes = function () {
+  myDashes = document.getElementById('buttons');
+  spaces = document.createElement('ul');
+
+  for (var i = 0; i < rndmWordArray.length; i++) {
+    letters.id = '';
+    list = document.createElement('li');
+    list.id = '';
+    list.innerHTML = rndmWordArray[i];
+    myButtons.appendChild(letters);
+    letters.appendChild(list);
+  }
+}
+
+
+  /*----- event listeners -----*/
+buttons();
 
   /*----- functions -----*/
 function refreshPage(){
@@ -37,6 +83,7 @@ console.log(rndmWordArray);
 function guessHandle() {
 if (rndmWordArray.contains(userInput)) {
     guesses.push(userInput);
+    winWordArray.push(userInput);
 
 } else {
  guesses.push(userInput);
@@ -48,8 +95,25 @@ if (rndmWordArray.contains(userInput)) {
 }
 
 function loseCondition(lives) {
-if (lives === 0);
+if (lives === 0){
 return alert('YOU HAVE LOST!');
+}
+
+}
+
+function winConditon() {
+if (rndmWordArray.length === winWordArray.length) 
+{ return alert('YOU HAVE WON!!! CONGRATS!')}
+
+}
+
+function wordReveal() {
+if(guesses.includes(userInput))
 
 
 }
+
+
+// if lives === (certain number, add picture to hangman)
+
+// 
