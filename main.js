d32d4e93ce95aad9399b6cf208ba21262b59ef3d
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-17 11:43:56 -0400

    added random word

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 
 
   /*----- state variables -----*/
+ 
 let guess ;  // -- current guess
 let guesses = []; // -- how many guesses are left
 let lives ; // -- how many lives are left
 let blank ; // -- the blank space used for the correct letters
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
+let dictionary = ["toyota", "honda", "subaru", "mazda"];
+var selectedWord;
+var rndmNum;
+var word = dictionary[Math.floor(Math.random() * dictionary.length)];
   /*----- cached elements  -----*/
 let livesLeftEl = document.getElementById("lives");
 let lettersEl = document.getElementById("letters");
@@ -16,9 +19,13 @@ let getHintEl = document.getElementById("hints");
 
 
   /*----- event listeners -----*/
-restartbtn.addEventListener(onClick="refreshPage()")
+
 
   /*----- functions -----*/
 function refreshPage(){
     window.location.reload();
 } 
+
+
+
+console.log(word);
