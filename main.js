b7d56d0b5ca05c763c0745e4288d5e67c87b22ed
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-21 11:51:56 -0400

    working on keyboard

--- main.js
+++ main.js
@@ -3,22 +3,21 @@
 
 
 /*----- constants -----*/
-
+const myButton = document.querySelector("#Q");
 
   /*----- state variables -----*/
 
 let guess ;  // -- current guess
 let guesses = []; // -- how many guesses are left
 let lives = 10 ; // -- how many lives are left
-let blank ; // -- the blank space used for the correct letters
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let dictionary = ["toyota", "honda", "subaru", "mazda"];
-var selectedWord; 
-var rndmNum;
-var word = dictionary[Math.floor(Math.random() * dictionary.length)];
-var winWordArray = [];
-var myButtons;
-var letters;
+let selectedWord; 
+let rndmNum;
+let word = dictionary[Math.floor(Math.random() * dictionary.length)];
+let winWordArray = [];
+let myButtons;
+let letters;
   /*----- cached elements  -----*/
 let livesLeftEl = document.getElementById("lives");
 let lettersEl = document.getElementById("letters");
@@ -28,19 +27,44 @@ let del = document.getElementById("del");
 let q = document.getElementsByClassName("Q");
 let charArray = word.split("");
 let dashes = document.getElementsByClassName("dashes");
+let answer = document.getElementById("blanks")
 // switch pics each time you lose a life
- var hangmanPic = function() {
+let hangmanPic = function() {
 if (lives === 5 ) {
 //<img src="https://hangman.doncolton.com/play/hang0.png"></img>
 }
 }
 
+
+
 // dynamically rendering elements
 
   /*----- event listeners -----*/
-
+myButton.addEventListener('click', () => {
+  console.log('Button Clicked!');
+});
 
   /*----- functions -----*/
+function generateButton() {
+   let buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
+   .split("")
+   .map(
+     (letter) =>
+     `<button class = "alphabetButtonJS"
+     id="${letter}"
+     >
+      ${letter}
+     </button>`
+   )
+   .join("");
+return buttonsHTML;
+
+
+}
+  let rndmWordArray = Array.from(word);
+
+
+  console.log(rndmWordArray);
 
 
 function refreshPage(){
@@ -50,29 +74,28 @@ q.onClick = function(){
   return Q='q'
 }
 
-let displayItem = word.replace(/./g, '<span class="dashes">_</span>')
-userInput.innerHTML = displayItem;
+
 // array.from
 
-let rndmWordArray = Array.from(word);
+
 
 console.log(word);
-console.log(rndmWordArray);
 
 
-function guessHandle() {
-if (rndmWordArray.contains(userInput)) {
-    guesses.push(userInput);
-    winWordArray.push(userInput);
 
-} else {
- guesses.push(userInput);
- lives--;
- console.log(lives)
-}
+//function guessHandle() {
+//if (rndmWordArray.contains(userInput)) {
+  //  guesses.push(userInput);
+    //winWordArray.push(userInput);
+
+// } else {
+//  guesses.push(userInput);
+//  lives--;
+//  console.log(lives)
+// }
 
 
-}
+
 
 function loseCondition(lives) {
 if (lives === 0){
