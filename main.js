6ac8fea8e341c57a46d1ce50675f4850496a7be6
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-21 13:13:38 -0400

    adding css to keybaord

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const container = document.getElementById("alphabetButtons")
 
 let guess ;  // -- current guess
 let guesses = []; // -- how many guesses are left
-let lives = 10 ; // -- how many lives are left
+let lives = 6 ; // -- how many lives are left
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let dictionary = ["toyota", "honda", "subaru", "mazda"];
 let selectedWord; 
